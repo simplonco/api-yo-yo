7d775be92f634b93f055a609d62b4c53d9687667
Yassin AGHANI <aghani.yassin@gmail.com>
2015-12-11 11:59:55 +0100

    Add stepper object

---
+++ lib/stepper.rb
@@ -0,0 +1,33 @@
+require 'arduino_firmata'
+
+class Stepper
+ 
+  ARDUINO = ArduinoFirmata.connect '/dev/tty.wchusbserialfd120'
+
+  PIN_STEP = 9
+  PIN_DIR = 8
+
+  ARDUINO.digital_write PIN_DIR, true
+  ARDUINO.digital_write PIN_STEP, false
+
+  def self.turn( opts = {} )
+    step = opts[:step] || 100
+    direction = opts[:direction] || :up
+    speed = opts[:speed] || 75
+    # Convert speed to pause time
+    time = (100 - speed) / 1000.0
+
+    if direction == :down
+      ARDUINO.digital_write PIN_DIR, false
+    else
+      ARDUINO.digital_write PIN_DIR, true
+    end
+
+    step.times do |n|
+      ARDUINO.digital_write PIN_STEP, true
+      sleep time
+      ARDUINO.digital_write PIN_STEP, false
+      sleep time
+    end
+  end
+end
